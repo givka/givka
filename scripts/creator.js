35f40958023230ad260b96d0e03ca5741be5e6c1
ketshi <>
2018-03-21 00:53:16 +0100

    add constructor

---
+++ scripts/creator.js
@@ -0,0 +1,85 @@
+const Handlebars = require('handlebars');
+const MovieDataBase = require('./tmdb');
+const $ = require('jquery');
+
+const mdb = new MovieDataBase();
+
+class Creator {
+  movie(list) {
+    return mdb.getMovies(list, 10).then((movies) => {
+      for (const movie of movies) {
+        const source = `    <div id="container" class="{{id}}">
+        <img src="{{src}}">
+        <div class="bottom">{{title}}</div>
+      </div>`;
+        // var divName = document.createElement("div");
+        // divName.innerHTML = result.title;
+        // divName.className = "bottom"
+
+        const template = Handlebars.compile(source);
+
+        const context = {
+          id: movie.id,
+          src: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          // title: movie.title,
+        };
+
+        const result = template(context);
+        const minC = this.minColumn();
+        document.getElementsByClassName(`columnId-${minC}`)[0]
+          .insertAdjacentHTML('beforeend', result);
+      }
+    });
+  }
+
+  minColumn() {
+    let min = 1e9;
+    let minColumn;
+    const nbrColumns = $('#row').children().length;
+
+    for (let c = 1; c <= nbrColumns; c += 1) {
+      const columnChildren = $(`.columnId-${c.toString()}`).children().length;
+      if (min > columnChildren) {
+        min = columnChildren;
+        minColumn = c;
+      }
+    }
+    return minColumn;
+  }
+
+  columns(nbrColumns) {
+    const source = `<h1 id = "title" > Movies</h1>
+      <div id="movies">
+        <div id="row">
+          {{#each items}}
+      <div id="column" class="{{agree_button}}"></div>
+          {{/ each}}
+      </div>
+      </div>`;
+    const template = Handlebars.compile(source);
+    const array = [...Array(nbrColumns).keys()];
+
+    for (const number of array) {
+      array[number] = {
+        name: number + 1,
+      };
+    }
+
+    const context = {
+      items: array,
+    };
+
+    Handlebars.registerHelper('agree_button', function () {
+      const name = Handlebars.escapeExpression(this.name);
+
+      return new Handlebars.SafeString(`columnId-${name}`);
+    });
+
+    const result = template(context);
+    document.getElementById('content').insertAdjacentHTML('afterbegin', result);
+    return result;
+  }
+}
+
+module.exports = Creator;
+
--- scripts/tmdb.js
+++ scripts/tmdb.js
@@ -1,79 +1,70 @@
-useStrict();
+const API_KEY = 'aa79a25e783821b082e1e241e41889db';
+const LANGUAGE = 'fr-FR';
+const rp = require('request-promise');
 
-var API_KEY = 'aa79a25e783821b082e1e241e41889db';
+class MovieDataBase {
+  getCollection(id) {
+    const url = `https://api.themoviedb.org/3/collection/${id}`;
+    const addRequest = 'images';
 
-class movieDataBase {
+    return this.constructor.getRequest(url, addRequest);
+  }
 
-    getCollection(id) {
+  getTV(id) {
+    const url = `https://api.themoviedb.org/3/tv/${id}`;
+    const addRequest = 'credits,images,videos,recommendations';
 
-        var url = 'https://api.themoviedb.org/3/collection/' + id
-        var append_to_response = 'images'
-        return this.getRequest(url, append_to_response)
+    return this.constructor.getRequest(url, addRequest);
+  }
+  getPeople(id) {
+    const url = `https://api.themoviedb.org/3/people/${id}`;
+    const addRequest = 'movie_credits,images';
+
+    return this.constructor.getRequest(url, addRequest);
+  }
+  getMovie(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}`;
+    const addRequest = 'credits,images,videos,recommendations';
+
+    return this.constructor.getRequest(url, addRequest);
+  }
+
+  static getRequest(url, addRequestAppend, nbrPage) {
+    const options = {
+      method: 'GET',
+      url,
+      qs: {
+        language: LANGUAGE,
+        api_key: API_KEY,
+        append_to_response: addRequestAppend,
+        include_image_language: 'en,null',
+        page: nbrPage,
+      },
+      body: '{}',
+      json: true,
+    };
+    return rp(options)
+      .then(response => response)
+      .catch((err) => {
+        throw err;
+      });
+  }
+
+  getMovies(list, number) {
+    const url = `https://api.themoviedb.org/3/movie/${list}`;
+    const PromiseArray = [];
+
+    for (let i = 1; i <= number; i += 1) {
+      PromiseArray.push(this.constructor.getRequest(url, null, i));
     }
-
-    getTV(id) {
-
-        var url = 'https://api.themoviedb.org/3/tv/' + id
-        var append_to_response = 'credits,images,videos,recommendations'
-
-        return this.getRequest(url, append_to_response)
-    }
-    getPeople(id) {
-
-        var url = 'https://api.themoviedb.org/3/people/' + id
-        var append_to_response = 'movie_credits,images'
-
-        return this.getRequest(url, append_to_response)
-
-    }
-    getMovie(id) {
-
-        var url = 'https://api.themoviedb.org/3/movie/' + id
-        var append_to_response = 'credits,images,videos,recommendations'
-
-        return this.getRequest(url, append_to_response)
-    }
-
-    getRequest(url, append_to_response, page) {
-        var options = {
-            method: 'GET',
-            url: url,
-            qs: {
-                language: LANGUAGE, api_key: API_KEY, append_to_response: append_to_response,
-                include_image_language: 'en,null', page: page
-            },
-            body: '{}',
-            json: true
-        };
-        return rp(options)
-            .then(function (response) {
-                return response
-            })
-            .catch(function (err) {
-                console.log(err);
-
-            });
-    }
-
-    getRequests(list,number) {
-
-        var url = 'https://api.themoviedb.org/3/movie/'+list
-        var PromiseArray = [];
-
-        for (let i = 1; i <= number; i++) {
-            PromiseArray.push(this.getRequest(url, null, i))
-        }
-        return Promise.all(PromiseArray)
-            .then(allResponses => {
-                 allResponses =allResponses.map(value => value.results);
-                 allResponses = [].concat(...allResponses)
-                return allResponses  
-            })
-    }
+    return Promise.all(PromiseArray)
+      .then((allResponses) => {
+        let results = allResponses.map(value => value.results);
+        results = [].concat(...results);
+        return results;
+      });
+  }
 }
 
-module.exports = movieDataBase;
+module.exports = MovieDataBase;
 
-function useStrict() {
-    'use strict';
-}
