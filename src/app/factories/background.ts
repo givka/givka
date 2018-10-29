b021cc75dff6e4ce0b879642949b9a4047b61d9a
ketshi <>
2018-10-29 20:44:08 +0100

    add filter at beginning

--- src/app/factories/background.ts
+++ src/app/factories/background.ts
@@ -24,14 +24,8 @@ export class Background {
     }
     const background = `url(${image})`;
     const rule = `.main-content::before {
-    z-index: -1 !important;
-    content: ' ' !important;
-    position: fixed !important;
     background-image: ${background} !important;
-    background-size: cover !important; 
-    will-change: transform !important;
-    filter: blur(10px);
-    transform: scale(${random(1, 2, true)}) !important;
+    transform: scale(${random(1.1, 1.5, true)}) !important;
   }`;
     this.styleSheet.insertRule(rule, 0);
   }
